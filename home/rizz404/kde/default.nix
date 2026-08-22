0a8a6e18b58d568e413d3cb8343788c9a12c267a
Rizqiansyah <rizzthenotable@gmail.com>
2026-08-22 15:17:33 +0700

    Add mpv and kde config in home manager

---
+++ home/rizz404/kde/default.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  xdg.configFile."dolphinrc".source = ./dolphinrc;
+  xdg.configFile."katerc".source = ./katerc;
+  xdg.configFile."kdeglobals".source = ./kdeglobals;
+  xdg.configFile."kglobalshortcutsrc".source = ./kglobalshortcutsrc;
+  xdg.configFile."kwinrc".source = ./kwinrc;
+  xdg.configFile."plasma-org.kde.plasma.desktop-appletsrc".source = ./plasma-org.kde.plasma.desktop-appletsrc;
+  xdg.configFile."plasmarc".source = ./plasmarc;
+  xdg.configFile."plasmashellrc".source = ./plasmashellrc;
+}
---
+++ home/rizz404/mpv/default.nix
@@ -0,0 +1,6 @@
+{ ... }:
+{
+  xdg.configFile."mpv/mpv.conf".source = ./mpv.conf;
+  xdg.configFile."mpv/input.conf".source = ./input.conf;
+  xdg.configFile."mpv/scripts/autoload.lua".source = ./scripts/autoload.lua;
+}
