bc62f2546fe5b80e1e89ce7086b3ee045a5d3d00
Rizqiansyah <rizzthenotable@gmail.com>
2026-08-23 14:54:54 +0700

    Update KDE configuration with Catppuccin theme and additional packages

--- home/rizz404/kde/default.nix
+++ home/rizz404/kde/default.nix
@@ -1,5 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  catppuccin-kde = pkgs.catppuccin-kde.override {
+    flavour = [ "mocha" "macchiato" "frappe" "latte" ];
+    accents = [ "blue" "rosewater" "pink" "mauve" "red" "peach" "green" "teal" "sapphire" "lavender" ];
+    winDecStyles = [ "modern" ];
+  };
+in
 {
+  home.packages = [
+    pkgs.papirus-icon-theme
+    catppuccin-kde
+  ];
+
   xdg.configFile."dolphinrc".source = ./dolphinrc;
   xdg.configFile."katerc".source = ./katerc;
   xdg.configFile."kdeglobals".source = ./kdeglobals;
