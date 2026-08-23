206146ebe765acbf35a2d582e79ecaac0df61a84
Rizqiansyah <rizzthenotable@gmail.com>
2026-08-23 21:01:05 +0700

    Add settings to keep outputs and derivations in Nix configuration

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -7,6 +7,8 @@
     ];
   nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
+  nix.settings.keep-outputs = true;
+  nix.settings.keep-derivations = true;
 
   environment.systemPackages = with pkgs; [
     git
