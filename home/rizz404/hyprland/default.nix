f4af9320548082c3609d34bd6777260e731f433b
Rizqiansyah <rizzthenotable@gmail.com>
2026-09-02 19:49:08 +0700

    Set configType to "lua" for Hyprland window manager

--- home/rizz404/hyprland/default.nix
+++ home/rizz404/hyprland/default.nix
@@ -4,6 +4,7 @@
     enable = true;
     xwayland.enable = true;
     package = null; # paket sudah disediakan system-wide lewat modules/desktop/hyprland.nix
+    configType = "lua";
     extraConfig = builtins.readFile ./hyprland.lua;
   };
   xdg.portal.config.common.default = "*";
