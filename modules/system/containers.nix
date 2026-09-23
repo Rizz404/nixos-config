da05cd5854d26b89d22b6574d98dfdfde81d0871
Rizqiansyah <rizzthenotable@gmail.com>
2026-09-23 11:14:21 +0700

    Add Waydroid nftables package support and update configuration syntax

--- modules/system/containers.nix
+++ modules/system/containers.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   virtualisation.podman.enable = true;
   virtualisation.containers.policy.default = [{ type = "insecureAcceptAnything"; }];
   virtualisation.waydroid.enable = true;
+
+  # Kernel >=6.17 nixpkgs default gak nyertain modul ip_tables lagi,
+  # tapi package waydroid biasa masih hardcode ke iptables-legacy yang butuh itu.
+  # waydroid-nftables pakai nft langsung, gak butuh ip_tables sama sekali.
+  virtualisation.waydroid.package = pkgs.waydroid-nftables;
 }
